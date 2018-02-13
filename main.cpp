0d041d3e31ccbe9d2132c0beb30c5e736dcc6825
TrevorOwens4 <towens5@mail.csuchico.edu>
2018-02-13 11:20:05 -0800

    Description of what this version does

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Author: Trevor Owens
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+  string firstName;
+  string lastName; 
 
   //get user input
-
+cout << "What is your first name?\n";
+cin >> firstName;
+cout << "What is your last name?\n";
+cin >> lastName;
   //tell fortune
 
   return 0;
