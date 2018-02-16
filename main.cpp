3e5d8c9c1724691851b6378bcb73d3bce33083a8
TrevorOwens4 <towens5@mail.csuchico.edu>
2018-02-15 16:34:10 -0800

    Description of what this version does

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main()
   string firstName;
   string lastName; 
   string nickname;
+  char firstLetter;
+  char lastNameLastLetter;
   int lucky;
 
   //get user input
@@ -22,6 +24,24 @@ cin >> lastName;
 cout << "Welcome," << firstName [0] << "." << lastName [0] << "." << ",here is your fortune...\n"; 
 cout << "Your lucky number is..." << lucky << endl; 
  
- 
+firstLetter = firstName [0];
+ if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' ||firstLetter == 'u' || firstLetter == 'A' || firstLetter == 'E' || firstLetter == 'I' || firstLetter == 'O' || firstLetter == 'U') 
+ {
+ cout << "you are destined to be famous!\n"; 
+ }
+else 
+{
+cout << "you should keep a low profile.\n";
+}
+
+lastNameLastLetter = lastName [lastName.length() - 1];
+if (lastNameLastLetter == 'a' || lastNameLastLetter == 'e' || lastNameLastLetter== 'i' || lastNameLastLetter == 'o' || lastNameLastLetter == 'u' || lastNameLastLetter == 'A' || lastNameLastLetter == 'E' || lastNameLastLetter == 'I' || lastNameLastLetter == 'O' || lastNameLastLetter == 'U') 
+{ 
+cout << "you have already met your true love.\n";
+}
+
+
+cout << "have a good day!\n";
+
  return 0;
   }
